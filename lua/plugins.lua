05cdbb26f876aaf0278fd2aac5184d1674b923b2
angryCar80 <ouabbasaymen@gmail.com>
2025-08-05 13:59:17 +0100

    Using neovim with tmux

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,17 @@
 return {
-  
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    config = function()
+      local map = vim.keymap.set
+      local opts = { noremap = true, silent = true }
+
+      -- Tmux navigation keymaps
+      map("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", opts)
+      map("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", opts)
+      map("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", opts)
+      map("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", opts)
+    end,
+  },
 }
+
